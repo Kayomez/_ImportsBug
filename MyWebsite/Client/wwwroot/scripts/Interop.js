9ca2c676826b2c0c67f000a2ba6f8e9a2b528961
Kayomez <55811985+Kayomez@users.noreply.github.com>
2020-08-18 12:04:23 +0100

    Upload

---
+++ MyWebsite/Client/wwwroot/scripts/Interop.js
@@ -0,0 +1,24 @@
+﻿window.BlazorExtensions = {
+    SetupMineSweeper: function (dotnet) {
+        var eles = document.getElementsByClassName("box");
+        Array.from(eles).forEach((el) => {
+            el.addEventListener("contextmenu", function (event) {
+                var isRightMB;
+
+                if (event.which == 3)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
+                    isRightMB = true;
+                else if (event.button == 2)  // IE, Opera 
+                    isRightMB = true;
+                if (isRightMB)
+                    dotnet.invokeMethodAsync('BoxRightClicked', el.id)
+                else
+                    dotnet.invokeMethodAsync('BoxLeftClicked', el.id);
+                event.preventDefault();
+            });
+            el.addEventListener("click", function (event) {
+                dotnet.invokeMethodAsync('BoxLeftClicked', el.id);
+                event.preventDefault();
+            });
+        });
+    }
+}
---
+++ MyWebsite/Shared/PageMakeup/Navigation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace MyWebsite.Shared.PageMakeup
+{
+    public class Navigation
+    {
+        public string Title { get; set; }
+        public string UrlTitle { get; set; }
+        public string[] Directory { get; set; } = new string[0];
+        public string UrlFull
+        {
+            get
+            {
+                string ret = "/";
+                foreach (var item in Directory) ret += $"{item}/";
+                return $"{ret}/{UrlTitle}";
+            }
+        }
+        public Navigation(string title, string urlTitle)
+        {
+            this.Title = title;
+            this.UrlTitle = UrlTitle;
+        }
+        
+        public Navigation(string title, string urlTitle, string[] directory) : this(title,urlTitle)
+        {
+            this.Directory = directory;
+        }
+        public static Navigation[] GetAll
+        {
+            get
+            {
+                return new Navigation[]
+                {
+                     new Navigation("Home", "home"),
+                     new Navigation("About", "About")
+                };
+            }
+        }
+    }
+}
